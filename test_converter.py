b5ec3d45a718c1c4be29ee46a03d376b39b54518
Yuragranovsky <yuragranovsky@gmail.com>
2025-11-01 16:55:18 +0200

    removing cache

--- test_converter.py
+++ test_converter.py
@@ -7,6 +7,7 @@ class TestConverter(TestCase):
             "Company":['Toyota','Toyota','Hundai', 'Hundai', 'Hundai' ],
             "Model": ['Camry', 'Corolla', 'i10', 'Elantra', 'Kona']
             }))
+        self.mapper = columnsMapper(["Company","Model"], self.df) 
     def test_enumerator(self):
         expected = {
             'Toyota': 0,
@@ -14,6 +15,17 @@ class TestConverter(TestCase):
         } 
         actual = enumerator(self.df['Company'].unique())  
         self.assertEqual(expected, actual) 
+    def test_columnsMapper(self) :
+        actual: dict[str, dict[str, int]] =columnsMapper(["Company"], self.df) 
+        expected:dict[str, dict[str, int]] = {
+            "Company":{"Toyota":0, "Hundai":1}
+            } 
+        self.assertEqual(expected, actual) 
+    def test_converterX(self) :
+        dfConverter = convertX(self.df,self.mapper)
+        actualDict = dfConverter.to_dict(orient="list")
+        expectedDict = {"Company":[0,0,1,1,1], "Model": [0, 1, 2, 3, 4]}
+        self.assertEqual(expectedDict, actualDict)   
 if __name__ == '__main__' :
     main()
        
