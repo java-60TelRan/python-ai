4ebeef1ad642b592635ea4bf8b6af9f2bc7e415b
Yuragranovsky <yuragranovsky@gmail.com>
2025-11-12 17:52:41 +0200

    HW #32 solution

---
+++ text_model.py
@@ -0,0 +1,19 @@
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy
+
+class TextModel:
+    def __init__(self, text: str):
+        text_arr = text.split('.')
+        self.text_model = [res for s in text_arr if (res := s.strip())]
+        self.vectorizer = TfidfVectorizer(stop_words="english")
+        self.embeddings_text = self.vectorizer.fit_transform(self.text_model)
+    def getAnswers(self, query: str, nAnswers: int) -> list[str]:
+        embeddings_query = self.vectorizer.transform([query])
+        sims:numpy = cosine_similarity(self.embeddings_text, embeddings_query)
+        simsFlat = sims.ravel()
+        indices = numpy.argsort(simsFlat)[::-1]  # reverse order
+        res =  [self.text_model[i] for i in indices[:nAnswers] if simsFlat[i]]
+        return res
+        
+  
